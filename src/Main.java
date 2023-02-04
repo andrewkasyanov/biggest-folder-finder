35130c8c94cd6dea7fb3b518f04da1292eacb295
Andrei Kasianov <kasyanov.rss@gmail.com>
2023-02-05 00:25:35 +0300

    dev

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,26 @@
-public class Main
-{
-    public static void main(String[] args)
-    {
+import java.io.File;
+import java.nio.file.Files;
+
+public class Main {
+    public static void main(String[] args) {
+        String folderpath = "C:\\Users\\kasya\\Desktop\\Java";
+        File file = new File(folderpath);
+        System.out.println(getFolderSize(file));
 
 
 
 
+    }
 
-
+    public static long getFolderSize(File folder) {
+        if(folder.isFile()) {
+            return folder.length();
+        }
+        long sum = 0;
+        File[] files = folder.listFiles();
+        for(File file :files) {
+            sum += getFolderSize(file);
+        }
+        return sum;
     }
 }
