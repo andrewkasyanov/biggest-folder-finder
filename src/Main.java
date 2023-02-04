86b9a442173204a6fead3fb7b2fa12aea6383848
Andrei Kasianov <kasyanov.rss@gmail.com>
2023-02-05 00:55:48 +0300

    dev

--- src/Main.java
+++ src/Main.java
@@ -1,24 +1,43 @@
 import java.io.File;
 import java.nio.file.Files;
+import java.util.concurrent.ForkJoinPool;
 
 public class Main {
     public static void main(String[] args) {
-        String folderpath = "C:\\Users\\kasya\\Desktop\\Java";
+
+//        MyThread thread = new MyThread(1);
+//        MyThread thread2 = new MyThread(2);
+//
+//        thread.start();
+//        thread2.start();
+
+
+
+
+
+        String folderpath = "F:\\ОБРАЗЫ";
         File file = new File(folderpath);
-        System.out.println(getFolderSize(file));
 
+        long start = System.currentTimeMillis();
+//        System.out.println(getFolderSize(file));
 
+        FolderSizeCalculator calculator = new FolderSizeCalculator(file);
+        ForkJoinPool pool = new ForkJoinPool();
+        long size = pool.invoke(calculator);
+        System.out.println(size);
 
+        long duration = (System.currentTimeMillis() - start);
+        System.out.println(duration + " ms");
 
     }
 
     public static long getFolderSize(File folder) {
-        if(folder.isFile()) {
+        if (folder.isFile()) {
             return folder.length();
         }
         long sum = 0;
         File[] files = folder.listFiles();
-        for(File file :files) {
+        for (File file : files) {
             sum += getFolderSize(file);
         }
         return sum;
